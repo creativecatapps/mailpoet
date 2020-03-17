41b16782fcd22e33f47ebc313febbd7e8f292fde
Amine Ben hammou <amine@mailpoet.com>
2020-03-17 15:15:21 +0100

    Add email content field
    [MAILPOET-2678]

--- assets/js/src/settings/pages/signup_confirmation/index.tsx
+++ assets/js/src/settings/pages/signup_confirmation/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import EnableSignupConfirmation from './enable_signup_confirmation';
 import EmailSubject from './EmailSubject';
+import EmailContent from './EmailContent';
+import ConfirmationPage from './ConfirmationPage';
 
 export default function SignupConfirmation() {
   return (
     <div className="mailpoet-settings-grid">
       <EnableSignupConfirmation />
       <EmailSubject />
+      <EmailContent />
     </div>
   );
 }
